7b9cf140842d159fd417e4ab8e7cb7d28ead63e2
Nathan Lambert <nlambert@mymail.mines.edu>
2020-02-02 21:36:26 -0700

    fully implemented exit builtin and have system for creating array of all of the arguments

--- mains/shell.c
+++ mains/shell.c
@@ -12,6 +12,7 @@
 #include "interpreter.h"
 
 void check_exit(struct ast_statement_list *statements);
+char** get_command_args(char* command, struct ast_argument_list *arglist);
 
 int main(int argc, char *argv[]){
 	// TODO: WRITE A MAIN 
@@ -33,37 +34,22 @@ int main(int argc, char *argv[]){
 			const char* const argv[] = {"exit", NULL}; //constant array of constant argument strings
 			builtin_command_get("exit")->function(interpreter_new(false), argv, 0, 1, 2);
 		}
-		
-		// char subbuff[5];
-		// memcpy(subbuff, &line[0], 4);
-		// subbuff[5] = '\0';
-		// if(strcmp(subbuff, "exit") == 0){
-		// 	printf("you want to exit\n");
-
-		// 	const char* const argv[] = {"exit", "6", "7", NULL}; //constant array of constant argument strings
-		// 	builtin_command_get("exit")->function(interpreter_new(true), argv, 0, 1, 2);
-		// }
 
 		struct ast_statement_list *statements = parse_input(line);
 		//struct ast_argument_list *arglist = statements->first->pipeline->first->arglist;
 		check_exit(statements);
 
 		ast_statement_list_free(statements);
-		
 		free(line);
 	}
-
-
 	//free memory before exiting
 	free(p);
-
-
 	return 0;
 }
 
-
+//check if the command was "exit"
 void check_exit(struct ast_statement_list *statements){
-	printf("in check exit\n");
+	//printf("in check exit\n");
 	int length_of_arg = statements->first->pipeline->first->arglist->first->parts->first->string->size;
 	if(length_of_arg != 4){
 		return;
@@ -73,14 +59,45 @@ void check_exit(struct ast_statement_list *statements){
 	subbuff[5] = '\0';
 	char test[] = "exit";
 	int i;
-	for(i = 0; i < 5; i++){
+	for(i = 0; i < 4; i++){
 		if(subbuff[i] != test[i]){
 			break;
 		}
 	}
-	if(i == 5){
-		printf("you want to exit\n");
-		const char* const argv[] = {"exit", "6", "7", NULL}; //constant array of constant argument strings
-		builtin_command_get("exit")->function(interpreter_new(true), argv, 0, 1, 2);
+	if(i == 4){
+		//printf("you want to exit\n");
+		char** args = get_command_args("exit", statements->first->pipeline->first->arglist);
+		builtin_command_get("exit")->function(interpreter_new(true), (const char* const*)args, 0, 1, 2);
+		free(args);
 	}
+	return;
+}
+
+
+/*
+ *function that takes in the command typed and creates a null terminated
+   array of strings of all of the arguemnts (including the command itself)
+*/
+char** get_command_args(char* command, struct ast_argument_list *arglist){
+	int capacity = 2;
+	int used = 1;
+	char **arr = malloc(sizeof(char*)*capacity);
+	arr[0] = command;
+	while(arglist->rest != NULL){
+		char* arg = arglist->rest->first->parts->first->string->data;
+		if(capacity == used){
+			capacity *= 2;
+			arr = realloc(arr, sizeof(char*) * capacity);
+		}
+		arr[used] = arg;
+		used++;
+		arglist = arglist->rest;
+	}
+	//add in terminating NULL
+	if(capacity == used){
+			capacity *= 2;
+			arr = realloc(arr, sizeof(char*) * capacity);
+	}
+	arr[used] = NULL;
+	return arr;
 }
